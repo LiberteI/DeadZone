cff3289348a4296d3cf459000bcba3e1fe4ffe10
yimingy <yimingy@cs.dal.ca>
2025-08-31 12:56:00 -0300

    Add primitive char switcher
    Add chars in inspector
    
    Press 1–9 to switch to desired char. Automatically validated

---
+++ Script/Global/SurvivorSwitchManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+public class SurvivorSwitchManager : MonoBehaviour
+{   
+    /*
+        This script is responsible for switching among / between survivors.
+    */
+    [SerializeField] private List<GameObject> survivorList;
+
+    void Update(){
+        TrySwitchSurvivor();
+    }
+
+    private void TrySwitchSurvivor(){
+        if(survivorList.Count <= 1){
+            return;
+        }
+        
+        int index = -99;
+        
+        // take an index
+        for(int i = 0; i < 10; i ++){
+            if(Input.GetKeyDown(KeyCode.Alpha0 + i)){
+                index = i;
+                break;
+            }
+        }
+        if(index == -99){
+            // no input return
+            return;
+        }
+        // validate input
+        if(NormalisedIndex(index) < 0 || NormalisedIndex(index) >= survivorList.Count){
+            Debug.Log("index out of scoop");
+            return;
+        }
+        ActiveSurvivor(NormalisedIndex(index));
+    }
+
+    private void ActiveSurvivor(int idx){
+        for(int i = 0; i < survivorList.Count; i ++){
+            SurvivorBase survivorScript = survivorList[i].GetComponentInChildren<SurvivorBase>();
+            if(idx == i){
+                // set this to be controlled by the player
+                survivorScript.parameter.isPlayedByPlayer = true;
+
+                Debug.Log($"Currently controlling {survivorList[idx]}");
+            }
+            else{
+                // set this to be controlled by AI
+                survivorScript.parameter.isPlayedByPlayer = false;
+            }
+        }
+    }
+
+    private int NormalisedIndex(int idx){
+        return idx - 1;
+    }
+
+
+
+}
